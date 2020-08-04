e5c33f59a2f3b3e3007133b0b7fce0410bca25db
Vinicius <viniciuslimaveiga11@gmail.com>
2020-08-04 01:03:39 -0300

    fix: change second button text

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -31,8 +31,8 @@ function Landing() {
                     </a>
 
                     <a href="" className="give-classes">
-                        <img src={giveClassesIcon} alt="Estudar" />
-                        Estudar
+                        <img src={giveClassesIcon} alt="Dar aula" />
+                        Dar aula
                     </a>
                 </div>
 
