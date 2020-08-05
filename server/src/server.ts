1a87753cb479ef4739d854fe9043e29dd94718a7
Vinicius <viniciuslimaveiga11@gmail.com>
2020-08-05 20:01:50 -0300

    feat(knex): create SQLite databases

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log("Hello World")
+import express from 'express';
+
+const app = express();
+
+app.use(express.json())
+
+//http://localhost:3333/users
+
+app.get('/', (request, response) => {
+
+    return response.json({ message: 'Hello World' });
+});
+
+app.listen(3333);
