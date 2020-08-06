937e92e81f8e91fe44925e4f761d3b66edf8a5a4
Vinicius <viniciuslimaveiga11@gmail.com>
2020-08-05 23:56:28 -0300

    feat: api connections with node.js

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesControllers = new ClassesController();
-routes.get('/classes', classesControllers.index );
-routes.post('/classes', classesControllers.create );
+const connetionsController = new ConnectionsController();
+
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connetionsController.index);
+routes.post('/connections', connetionsController.create);
+
 
 export default routes;
