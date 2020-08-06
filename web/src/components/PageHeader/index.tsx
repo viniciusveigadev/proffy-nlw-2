f54d0b78d7d1584f5608400c3e848673dfd4c2ef
Vinicius <viniciuslimaveiga11@gmail.com>
2020-08-06 15:28:40 -0300

    style: refactor front-end of mobile and desktop

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,7 +8,8 @@ import backIcon from '../../assets/images/icons/back.svg'
 import './styles.css';
 
 interface PageHeaderProps {
-    title: string; //Props. with: ? (e.g.: title?:) means it isn't mandatory.
+    title: string;
+    description?: string; //Props. with: ? (e.g.: title?:) means it isn't mandatory.
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -23,6 +24,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+                {props.description && <p>{props.description}</p>}
 
                 {props.children}
             </div>
