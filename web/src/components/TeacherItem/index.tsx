2b8dbe2159a5af6a25e2190b177365114e914cac
Vinicius <viniciuslimaveiga11@gmail.com>
2020-08-07 04:49:12 -0300

    feat: implement give class page, refactor totalconnections

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,49 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://img.icons8.com/plasticine/2x/walter-white.png" alt="Walter White" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Walter White</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias avançadas de Química.
-            <br /><br />
-            Apaixonado por ensinar pessoas a fazer dinheiro. Alguns me chamam por Heisenberg
-        </p>
+            <p> {teacher.bio} </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$: 800,00</strong>
+                    <strong>R$: {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_black" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp" />
                 Entrar em contato
-            </button>
+                </a>
             </footer>
         </article>
     );
