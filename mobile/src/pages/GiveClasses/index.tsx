fcd219da67108216e874d2aca9ea9c76e0590d95
Vinicius <viniciuslimaveiga11@gmail.com>
2020-08-13 17:58:24 -0300

    feat: finish mobile navigation

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -8,29 +8,28 @@ import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
 function GiveClasses() {
-    const {goBack} = useNavigation();
+  const { goBack } = useNavigation();
 
-    function handlerNavigationBack(){
-        goBack();
-    }
-    return (
-        <View style={styles.container} >
-            <ImageBackground
-                resizeMode="contain"
-                source={giveClassesBgImage}
-                style={styles.content}
-            >
-                <Text style={styles.title}>Quer ser um Proffy?</Text>
-                <Text style={styles.description}>
-                    Para começar, você precisa se cadastrar como professor na nossa plataforma web.
-            </Text>
-            </ImageBackground>
+  function handleNavigateBack() {
+    goBack();
+  }
 
-            <RectButton onPress={handlerNavigationBack} style={styles.okButton}>
-                <Text style={styles.okButtonText}>Tudo bem!</Text>
-            </RectButton>
-        </View>
-    );
+  return <View style={styles.container} >
+    <ImageBackground
+      resizeMode="contain"
+      source={giveClassesBgImage}
+      style={styles.content}
+    >
+      <Text style={styles.title}>Quer ser um Proffy?</Text>
+      <Text style={styles.description}>
+        Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+      </Text>
+    </ImageBackground>
+
+    <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+      <Text style={styles.okButtonText}>Tudo bem</Text>
+    </RectButton>
+  </View>;
 }
 
 export default GiveClasses;
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useIsFocused } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -11,7 +13,17 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png'
 import heartIcon from '../../assets/images/icons/heart.png'
 
 function Landing() {
+  const isFocused = useIsFocused();
   const { navigate } = useNavigation();
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    })
+  }, [isFocused]);
 
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses')
@@ -51,7 +63,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas {' '}
+        Total de {totalConnections} conexões já realizadas {' '}
         <Image source={heartIcon} />
       </Text>
     </View>
